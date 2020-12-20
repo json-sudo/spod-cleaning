415b0ac1e3e1dbc3abba4b68b3e70d3286c2ed54
Jason <jasonjasyno50@gmail.com>
2020-12-20 14:03:49 +0100

    splitting page routes with react suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, Suspense, lazy } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import DrawerComponent from './components/drawer/drawer.component'
 import Footer from './components/footer/footer.component'
 import Header from './components/header/header.component'
+import Loader from './components/loader/loader.component'
 
-import AboutUsPage from './pages/about-us/about-us.page'
-import ChemicalSalesPage from './pages/chemical-sales/chemical-sales.page'
-import CleaningServicesPage from './pages/cleaning-services/cleaning-services.page'
-import ContactUsPage from './pages/contact-us/contact-us.page'
-import EquipmentRentalsPage from './pages/equipment-rentals/equipment-rentals.page'
+// import AboutUsPage from './pages/about-us/about-us.page'
+// import ChemicalSalesPage from './pages/chemical-sales/chemical-sales.page'
+// import CleaningServicesPage from './pages/cleaning-services/cleaning-services.page'
+// import ContactUsPage from './pages/contact-us/contact-us.page'
+// import EquipmentRentalsPage from './pages/equipment-rentals/equipment-rentals.page'
+// import HomePage from './pages/home/home.page'
+// import ServicesPage from './pages/services/services.page'
 import Error404Page from './pages/page-not-found/error-404.page'
-import HomePage from './pages/home/home.page'
-import ServicesPage from './pages/services/services.page'
 
 import './App.css'
+
+const AboutUsPage = lazy(() => import('./pages/about-us/about-us.page'))
+const ChemicalSalesPage = lazy(() => import('./pages/chemical-sales/chemical-sales.page'))
+const CleaningServicesPage = lazy(() => import('./pages/cleaning-services/cleaning-services.page'))
+const ContactUsPage = lazy(() => import('./pages/contact-us/contact-us.page'))
+const EquipmentRentalsPage = lazy(() => import('./pages/equipment-rentals/equipment-rentals.page'))
+const HomePage = lazy(() => import('./pages/home/home.page'))
+const ServicesPage = lazy(() => import('./pages/services/services.page'))
+
 
 export const AppContext = createContext({})
 
@@ -32,16 +42,18 @@ function App() {
 
 				<DrawerComponent />
 
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/about-us" component={AboutUsPage} />
-					<Route path="/contact-us" component={ContactUsPage} />
-					<Route path="/services" component={ServicesPage} />
-					<Route path="/chemical-sales" component={ChemicalSalesPage} />
-					<Route path="/equipment-rentals" component={EquipmentRentalsPage} />
-					<Route path="/cleaning-services" component={CleaningServicesPage} />
-					<Route component={Error404Page} />
-				</Switch>
+				<Suspense fallback={<Loader loading={true} />}>
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/about-us" component={AboutUsPage} />
+						<Route path="/contact-us" component={ContactUsPage} />
+						<Route path="/services" component={ServicesPage} />
+						<Route path="/chemical-sales" component={ChemicalSalesPage} />
+						<Route path="/equipment-rentals" component={EquipmentRentalsPage} />
+						<Route path="/cleaning-services" component={CleaningServicesPage} />
+						<Route component={Error404Page} />
+					</Switch>
+				</Suspense>
 
 				<Footer />
 			</div>
--- src/assets/svg-components/instagram.icon.jsx
+++ src/assets/svg-components/instagram.icon.jsx
@@ -5,9 +5,9 @@ const InstagramIcon = ({ title }) => {
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <title>{title}</title>
             <linearGradient id="a" gradientTransform="matrix(0 -1.982 -1.844 0 -132.522 -51.077)" gradientUnits="userSpaceOnUse" x1="-37.106" x2="-26.555" y1="-72.705" y2="-84.047">
-                <stop offset="0" stop-color="#fd5"/>
-                <stop offset=".5" stop-color="#ff543e"/>
-                <stop offset="1" stop-color="#c837ab"/>
+                <stop offset="0" stopColor="#fd5"/>
+                <stop offset=".5" stopColor="#ff543e"/>
+                <stop offset="1" stopColor="#c837ab"/>
             </linearGradient>
 
             <path d="M1.5 1.633C-.386 3.592 0 5.673 0 11.995c0 5.25-.916 10.513 3.878 11.752 1.497.385 14.761.385 16.256-.002 1.996-.515 3.62-2.134 3.842-4.957.031-.394.031-13.185-.001-13.587-.236-3.007-2.087-4.74-4.526-5.091C18.89.029 18.778.005 15.91 0 5.737.005 3.507-.448 1.5 1.633z" fill="url(#a)"/>
