3d7a9b7a45336dd7b108a8143c960bbf19504862
Jason <jasonjasyno50@gmail.com>
2020-12-20 13:37:53 +0100

    adding a transition to toggling the drawer component

--- src/components/drawer/drawer.component.jsx
+++ src/components/drawer/drawer.component.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Transition } from 'react-transition-group'
 
 import { AppContext } from '../../App'
 
@@ -23,45 +24,51 @@ const DrawerComponent = () => {
     return (
         <>
             {backdrop}
-            
-            <div className={`drawer-container ${drawerIsOpen ? 'open' : ''}`}>
-                <header className="drawer-header">
-                    <span>Spod Integrated</span>
-                    
-                    <button 
-                        className="close-btn"
-                        aria-label="Close navigation drawer"
-                        onClick={() => closeDrawer()}
-                    >
-                        &times;
-                    </button>
-                </header>
 
-                <nav>
-                    <ul className="drawer-nav">
-                        <li onClick={() => closeDrawer()}><Link to="/">Home</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/services">Services</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/about-us">About Us</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/contact-us">Contact Us</Link></li>
-                    </ul>
-                </nav>
+            <Transition in={drawerIsOpen} timeout={300}>
+                {
+                    state => (
+                        <div className={`drawer-container drawer-container-${state}`}>
+                            <header className="drawer-header">
+                                <span>Spod Integrated</span>
 
-                <hr />
+                                <button
+                                    className="close-btn"
+                                    aria-label="Close navigation drawer"
+                                    onClick={() => closeDrawer()}
+                                >
+                                    &times;
+                                </button>
+                            </header>
 
-                <nav>
-                    <ul className="drawer-nav">
-                        <li onClick={() => closeDrawer()}>
-                            <span 
-                                onClick={() => requestService()}
-                                role="link"
-                                tabIndex={0}
-                            >
-                                Request a service now
-                            </span>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+                            <nav>
+                                <ul className="drawer-nav">
+                                    <li onClick={() => closeDrawer()}><Link to="/">Home</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/services">Services</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/about-us">About Us</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/contact-us">Contact Us</Link></li>
+                                </ul>
+                            </nav>
+
+                            <hr />
+
+                            <nav>
+                                <ul className="drawer-nav">
+                                    <li onClick={() => closeDrawer()}>
+                                        <span
+                                            onClick={() => requestService()}
+                                            role="link"
+                                            tabIndex={0}
+                                        >
+                                            Request a service now
+                                        </span>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    )
+                }
+            </Transition>
         </>
     )
 }
